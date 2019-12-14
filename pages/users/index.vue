6144ae205634acfc69fcf9205aa9670be5912b5a
Evgen904 <evgen904@gmail.com>
2019-12-14 22:57:14 +0300

    Динамические роуты и валидация

---
+++ pages/users/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <h1>Users page</h1>
+    
+    <ul v-for="user in users" :key="item">
+      <li>
+        <a href="#" @click="goTo(user)">User {{ user }}</a>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    users: [
+      1, 2, 3, 4, 5 
+    ]
+  }),
+  methods: {
+    goTo(user) {
+      this.$router.push('/users/' + user);
+    }
+  }
+}
+</script>
